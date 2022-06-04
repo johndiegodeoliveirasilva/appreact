49094cbcc9f50cce53446f9270c0ce53fd779fe9
John Diego <johnoliveirasilva9@gmail.com>
2022-06-04 12:32:45 -0300

    variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Meu app</div>;
+  const random = Math.random();
+  const active = false
+  const title = <h1>Esse é um titulo</h1>
+  return (
+    <>
+      {title}
+      <p className={active ? 'active': 'inactive'}>{(random * 100)/ 50}</p>
+    </>
+    );
 }
 
 export default App;
