0c18d6c54427c16b0cc6fb9569e95abf9779668c
John Diego <johnoliveirasilva9@gmail.com>
2022-06-05 11:37:35 -0300

    function, style, variables

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 
 const App = () => {
   const random = Math.random();
-  const active = false
-  const title = <h1>Esse é um titulo</h1>
+
+  const active = false;
+
+  const title = <h1>Esse é um titulo</h1>;
+
+  const styleH1 = {
+    color: 'blue',
+    fontSize: '20px',
+    fontFamily: 'Helvetica',
+  }
+
+  function showName(sobrenome) {
+    return 'André' + sobrenome;
+  }
+
   return (
     <>
       {title}
+      {showName()}
+      <h1 style={styleH1}>asdad</h1>
       <p className={active ? 'active': 'inactive'}>{(random * 100)/ 50}</p>
     </>
-    );
+  );
 }
 
 export default App;
