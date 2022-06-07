8e06dede14f742c362b42e48e2e7af241ed455f3
John Diego <johnoliveirasilva9@gmail.com>
2022-06-07 14:14:52 -0300

    jsx array 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const App = () => {
-  const filmes = ['Before Sunrise', 'Before Sunset', 'Before Midnight'];
+  const books = [
+    { name: 'A Game of Thrones', year: 1996 },
+    { name: 'A Clash of Kings', year: 1998},
+    { name: 'A Storm of Swords', year: 2000 },
+  ];
   return (
     <ul>
-      {filmes.map((filme) => (
-        <li key={filme}>{filme}</li>
+      {books
+      .filter(({ year }) => year >= 1998)
+      .map(({name, year}) => (
+        <li key={name}>{name}, {year}</li>
       ))}
     </ul>
   );
