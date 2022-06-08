699e525a75b52c5de0dad797b0514ab3cac55ba7
John Diego <johnoliveirasilva9@gmail.com>
2022-06-08 14:16:54 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
 import React from 'react';
-// Mostrar só os produtos acima de 1500 reais
+import Footer from './Footer';
+import Form from './Form/Form';
+import Header from './Header';
 
-const products = [
-  {
-    id: 1,
-    name: 'Smartphone',
-    price: 'R$ 2000',
-    colors: ['#29d8d5', '#252a34', '#fc3766']
-  },
-  {
-    id: 2,
-    name: 'Notebook',
-    price: 'R$ 3000',
-    colors: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    name: 'Tablet',
-    price: 'R$ 1500',
-    colors: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
+const Teste = () => {
+  const active = false;
+  if (active) {
+    return <p>Teste</p>;
+  } else {
+    return null;
+  }
+}
 
 const App = () => {
-  const data = products.filter(({ price }) => parseInt(price.replace("R$ ", '')) > 1500)
-
   return (
-    <section>
-      {
-      data.map(({id, name, price, colors}) => (
-        <div key={id}>
-          <h1 >{name}</h1>
-          <p>Price: {price}</p>
-          <ul>
-            {colors.map((color) => (
-              <li key={color} style={{ backgroundColor: color, listStyleType:"none", color: 'white'}}>{color}</li>
-              ))}
-          </ul>
-        </div>
-)) }
-    </section>
+    <>
+      <Teste/>
+      <Header/>
+      <Form/>
+      <Footer/>
+    </>
   );
 };
 
