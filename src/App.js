b88cd12d445b665c56fff61b774bfe1b8723596a
John Diego <johnoliveirasilva9@gmail.com>
2022-06-08 13:33:54 -0300

    events 1

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 import React from 'react';
-// Mostrar só os produtos acima de 1500 reais
-
-const products = [
-  {
-    id: 1,
-    name: 'Smartphone',
-    price: 'R$ 2000',
-    colors: ['#29d8d5', '#252a34', '#fc3766']
-  },
-  {
-    id: 2,
-    name: 'Notebook',
-    price: 'R$ 3000',
-    colors: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    name: 'Tablet',
-    price: 'R$ 1500',
-    colors: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
 
 const App = () => {
-  const data = products.filter(({ price }) => parseInt(price.replace("R$ ", '')) > 1500)
-
+  function handleScroll(event) {
+    console.log(event.target)
+  }
+  window.addEventListener('scroll', handleScroll);
   return (
-    <section>
-      {
-      data.map(({id, name, price, colors}) => (
-        <div key={id}>
-          <h1 >{name}</h1>
-          <p>Price: {price}</p>
-          <ul>
-            {colors.map((color) => (
-              <li key={color} style={{ backgroundColor: color, listStyleType:"none", color: 'white'}}>{color}</li>
-              ))}
-          </ul>
-        </div>
-)) }
-    </section>
+    <div style={{ height: '200vh' }}>
+      <button onClick={(event) => alert(event.target.innerText)}>
+        Compre Violão
+      </button>
+    </div>
   );
 };
 
