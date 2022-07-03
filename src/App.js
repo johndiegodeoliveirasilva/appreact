dfe0e6b81e76736edb9f274ca33ba39a16902b8b
John Diego <johnoliveirasilva9@gmail.com>
2022-07-03 14:10:31 -0300

    init useState

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ const App = () => {
   const [active, setActve] = React.useState(false);
   const [data, setData] = React.useState({ name: 'André', year: 30 });
 
-  
   function handleClick() {
     setActve(!active)
+    setData({...data,  faculdade: "Possui faculdade"})
   }
-  
+   
   return (
     <>
-    <p>{data.name}</p>
-    <p>{data.year}</p>
+    <p>{ data.name }</p>
+    <p>{ data.year }</p>
+    <p>{ active ? data.faculdade : "" }</p>
     <button onClick={handleClick}>{active ? "Active" : "Inactive"}</button>
     </>
   );
