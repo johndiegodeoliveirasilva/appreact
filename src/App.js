d0198193168924cdc1ecc9aaaaeab700f63363d5
John Diego <johnoliveirasilva9@gmail.com>
2022-07-03 14:50:13 -0300

    useState1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
+import ButtonModal from './ButtonModal';
+import Modal from './Modal';
 
 const App = () => {
-  const [active, setActve] = React.useState(false);
-  const [data, setData] = React.useState({ name: 'André', year: 30 });
+  const [modal, setModal] = React.useState(false);
 
-  function handleClick() {
-    setActve(!active)
-    setData({...data,  faculdade: "Possui faculdade"})
-  }
-   
   return (
     <>
-    <p>{ data.name }</p>
-    <p>{ data.year }</p>
-    <p>{ active ? data.faculdade : "" }</p>
-    <button onClick={handleClick}>{active ? "Active" : "Inactive"}</button>
+    <Modal modal={modal} setModal={setModal}/>
+    <ButtonModal setModal={setModal}></ButtonModal>
     </>
   );
 };
